2752b52b2755c15d79885cf605e34a78fb57c783
Innocent1312 <anthonydev33@gmail.com>
2020-02-03 18:29:42 +0200

    Updates

---
+++ precache-manifest.5b83b4f72c37f50850122543f5d6c959.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8936084e79abac190931540b6b2b60db",
+    "url": "/react-typesctipt-todo-app/index.html"
+  },
+  {
+    "revision": "a3f9fb831dbee9b00fdd",
+    "url": "/react-typesctipt-todo-app/static/css/main.8ec52a7d.chunk.css"
+  },
+  {
+    "revision": "9a860e31e97625f08bc2",
+    "url": "/react-typesctipt-todo-app/static/js/2.4fbc3ecb.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-typesctipt-todo-app/static/js/2.4fbc3ecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3f9fb831dbee9b00fdd",
+    "url": "/react-typesctipt-todo-app/static/js/main.f0e82fb9.chunk.js"
+  },
+  {
+    "revision": "8f7ce2ed08c38f6eb81c",
+    "url": "/react-typesctipt-todo-app/static/js/runtime-main.3060d9dc.js"
+  }
+]);
